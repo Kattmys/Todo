f8a6da90d5b7dad13f705928943695ee08ed017a
Cicada1997 <francis.backstrom@gmail.com>
2025-04-02 19:34:07 +0200

    major style upgrade

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import flask
 import json
 import traceback
@@ -32,16 +33,24 @@ def post(lista, metod):
         r = json.loads(flask.request.data.decode("utf-8"))
 
         match metod:
-            case "add_item":    todo[r["index"]]["list"].append(r["value"])
+            case "add_item":    
+                todo[r["index"]]["list"].append(r["value"])
+                todo[r["index"]]["created"] = datetime.now().strftime('%Y-%m-%d %H:%M')
+
             case "add_list":    todo.append({"name": r["name"], "list": []})
-            case "move_item":   todo[r["list"]]["list"].insert(r["new_index"], todo[r["list"]]["list"].pop(r["old_index"]))
+            case "move_item":   todo[r["list"]]["lists"]["list"].insert(r["new_index"], 
+                                todo[r["list"]]["lists"]["list"].pop(r["old_index"]))
             case "move_list":   todo.insert(r["new_index"], todo.pop(r["old_index"]))
-            case "remove_item": del todo[r["list_index"]]["list"][r["item_index"]]
+            case "remove_item": del todo[r["list_index"]]["lists"]["list"][r["item_index"]]
             case "remove_list": del todo[r["index"]]
-            case "rename_item": todo[r["list_index"]]["list"][r["item_index"]] = r["name"]
+            case "rename_item": todo[r["list_index"]]["lists"]["list"][r["item_index"]] = r["name"]
             case "rename_list": todo[r["index"]]["name"] = r["name"]
             case _:
                 return "unknown command"
+
+        # now = datetime.now()
+        
+        todo[r["index"]]["last_edited"] = datetime.now().strftime('%Y-%m-%d %H:%M')
 
         save_data()
         return "success"
